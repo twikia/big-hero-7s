6f017a194f315c04b9e0ae2f4c7eca6f9e9128e3
tai pol <123347080+tai-pol@users.noreply.github.com>
2025-02-14 12:27:23 -0700

    quick update (works pretty well)

--- lab3/controllers/lab_3_ctrl/lab_3_ctrl.py
+++ lab3/controllers/lab_3_ctrl/lab_3_ctrl.py
@@ -5,10 +5,6 @@ import math
 from controller import Robot, Motor, DistanceSensor, Supervisor # type: ignore
 import numpy as np
 
-pose_x = 0
-pose_y = 0
-pose_theta = 0
-
 # create the Robot instance.
 robot = Supervisor()
 
@@ -50,23 +46,37 @@ compass.enable(SIM_TIMESTEP)
 
 # TODO: Find waypoints to navigate around the arena while avoiding obstacles
 # x, y, theta stored in array of tuples
-waypoints = [(.04, .5, 0) ]
+waypoints = [(-0.304705, -.004838, 0),
+             (-0.224705, -0.004838, math.pi/2),
+             (-.194705, .285162, math.pi),
+             (-.304705, .285162,math.pi/2),
+             (-0.304705, 0.425162, 0),
+             (0.125295, 0.425162, - math.pi/4), #  top right corner
+             (0.345295, 0.265162, - math.pi/2 - math.pi/4),
+             (.045295, -0.014838, -math.pi/2 + math.pi/4),
+             (0.325295, -0.244838, -math.pi/2),
+             (0.325295, -0.414838, math.pi), #  bottom right corner
+             (-0.304705, -0.414838, math.pi/2),
+             (-0.304705, -0.194838, math.pi/2)
+             ]
 # Index indicating which waypoint the robot is reaching next
 index = 0
 
 # Get ping pong ball marker that marks the next waypoint the robot is reaching
-marker = robot.getFromDef("marker").getField("translation")
+# marker = robot.getFromDef("marker").getField("translation")
 
 
-# our random variables
+# our state variables / other random stuff we might need
 state = 0
 is_proportional_controller = True
+is_proportional_feedback_controller_state = True
+
 
 def reach_position(distance_to_goal, is_proportional=True) -> bool:
     """goes foward until the distance is within the error -> ruturns true is it has reached the goal"""
     global leftMotor, rightMotor, leftMax, rightMax
     
-    # turning variables r here
+    # tuning variables r here
     err_margin = .01
     foward_speed = .5
     min_foward_speed = .01
@@ -94,7 +104,7 @@ def turn_to_goal(ang_to_goal: float, is_proportional=True) -> bool:
     
     global leftMotor, rightMotor, leftMax, rightMax
     
-    # turning variables r here
+    # tuning variables r here
     err_margin = .01
     turn_speed = .2 # default value will change this
     min_turn_speed = .01
@@ -126,7 +136,13 @@ def turn_to_goal(ang_to_goal: float, is_proportional=True) -> bool:
 
 # Main Control Loop:
 def main():
-    global state
+    global state, index, gsr
+    
+    # local vars that don't need to be global:
+    pose_x = 0
+    pose_y = 0
+    pose_theta = 0
+    
     
     while robot.step(SIM_TIMESTEP) != -1:
         
@@ -159,7 +175,7 @@ def main():
         
         
         # Set the position of the marker
-        marker.setSFVec3f([waypoints[index][0], waypoints[index][1], 0.01])
+        # marker.setSFVec3f([waypoints[index][0], waypoints[index][1], 0.01])
         
         # Read ground sensor values
         for i, gs in enumerate(ground_sensors):
@@ -174,27 +190,27 @@ def main():
         
         # euclidian distance
         goal_pos = (-0.19, 0.125162, 0)
+        goal_pos = waypoints[index]
         euc_dis = math.pow(goal_pos[0] - pose_x, 2)
         euc_dis += math.pow(goal_pos[1] - pose_y, 2)
         euc_dis = math.sqrt(euc_dis)
         
         # calculate the angle to goal
         ang_to_goal = math.atan2(goal_pos[1] - pose_y, goal_pos[0] - pose_x)
-        ang_to_goal = ang_to_goal - pose_theta
-        
-        heading_to_goal_heading = goal_pos[2] - pose_theta
+        ang_to_goal = (ang_to_goal - pose_theta + math.pi) % (2 * math.pi) - math.pi
+        # heading_to_goal_heading = goal_pos[2] - pose_theta
+        heading_to_goal_heading = (goal_pos[2] - pose_theta + math.pi) % (2 * math.pi) - math.pi
         
         
         #############################################################
         # moving and printing stuff out
         #############################################################
         
-        # print("Current pose: [%5f, %5f, %5f]" % (pose_x, pose_y, pose_theta))
+        print("Current pose: [%5f, %5f, %5f]" % (pose_x, pose_y, pose_theta))
         print("euc distance: ", euc_dis)
         print("angle_to_goal: ", ang_to_goal)
         print("heading_to_goal: ", heading_to_goal_heading)
         print(state)
-        
         
         
         # perfect code here just wanted more accurate code dunno if that's allowed
@@ -211,15 +227,29 @@ def main():
         
         # idk if we need two states but i am just toggling using a bool:
         # more the robot depending on states
-        match state:
-            case 0:
-                if turn_to_goal(ang_to_goal, is_proportional_controller):
-                    state += reach_position(euc_dis, is_proportional_controller)
-            case 1:
-                state += turn_to_goal(heading_to_goal_heading, is_proportional_controller)
-            case _:
-                leftMotor.setVelocity(0)
-                rightMotor.setVelocity(0)
+        if is_proportional_feedback_controller_state:
+            pass
+        else:
+            match state:
+                case 0:
+                    if turn_to_goal(ang_to_goal, is_proportional_controller):
+                        state += reach_position(euc_dis, is_proportional_controller)
+                        
+                case 1:
+                    if turn_to_goal(heading_to_goal_heading, is_proportional_controller):
+                        state = 0
+                        index += 1
+                    
+                case _:
+                    leftMotor.setVelocity(0)
+                    rightMotor.setVelocity(0)
+        
+        
+        # exit condition here so that it ends
+        if index >= len(waypoints):
+            leftMotor.setVelocity(0)
+            rightMotor.setVelocity(0)
+            exit(0)
 
 
 
